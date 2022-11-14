a6cb86ee30c2e6e1a2fcf6566b70247c07c2cefb
Renuka <renukaboorsu@gmail.com>
2022-11-14 16:56:00 +0530

    Division is added

--- calculator.cpp
+++ calculator.cpp
@@ -11,6 +11,7 @@ class cal{
 		int add(int,int);
 		int sub(int,int);
 		int mul(int,int);
+		int div(int,int);
 };
 int cal::add(int x,int y){
 	return x+y;
@@ -21,10 +22,14 @@ int cal::sub(int x,int y){
 int cal::mul(int x,int y) {
 	return x*y;
 }
+int cal::div(int x,int y){
+	return x/y;
+}
 int main(){
 	cal obj;
 	cout<<"Addition of 2 num is "<<obj.add(6,4)<<endl;
 	cout<<"subtraction of 2 num is "<<obj.sub(6,4)<<endl;
 	cout<<"Multiplication of 2 num is "<<obj.mul(6,4)<<endl;
+	cout<<"Division of 2 num is "<<obj.div(6,4)<<endl;
 	return 0;
 }

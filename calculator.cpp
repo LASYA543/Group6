b54c18418eb586d83900c740858e4bbe9353fc7b
AkshithaB12 <benikiminnu@gmail.com>
2022-11-14 16:18:44 +0530

    sub is added

--- calculator.cpp
+++ calculator.cpp
@@ -9,12 +9,17 @@ class cal{
 			y=0;
 		}
 		int add(int,int);
+		int sub(int,int);
 };
 int cal::add(int x,int y){
 	return x+y;
 }
+int cal::sub(int x,int y){
+	return x-y;
+}
 int main(){
 	cal obj;
 	cout<<"Addition of 2 num is "<<obj.add(6,4)<<endl;
+	cout<<"subtraction of 2 num is "<<obj.sub(6,4)<<endl;
 	return 0;
 }

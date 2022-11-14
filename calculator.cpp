0df9e0b0622632ee94312efc4fcaf0c23c12b394
keerthi-200 <keerthi0268@gmail.git cinfig --listgit config --global user.email keerthi0268@gmail>
2022-11-14 16:29:05 +0530

    Mul is added

--- calculator.cpp
+++ calculator.cpp
@@ -10,6 +10,7 @@ class cal{
 		}
 		int add(int,int);
 		int sub(int,int);
+		int mul(int,int);
 };
 int cal::add(int x,int y){
 	return x+y;
@@ -17,9 +18,13 @@ int cal::add(int x,int y){
 int cal::sub(int x,int y){
 	return x-y;
 }
+int cal::mul(int x,int y) {
+	return x*y;
+}
 int main(){
 	cal obj;
 	cout<<"Addition of 2 num is "<<obj.add(6,4)<<endl;
 	cout<<"subtraction of 2 num is "<<obj.sub(6,4)<<endl;
+	cout<<"Multiplication of 2 num is "<<obj.mul(6,4)<<endl;
 	return 0;
 }
